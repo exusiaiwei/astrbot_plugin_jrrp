21e18770801cd2cdc2f55171aca58e25c359f08f
wzc <exusiai.wei@outlook.com>
2025-03-26 18:48:57 +0100

    将 Hello World 插件更改为人品插件，添加随机运势消息功能

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
-
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+import random
+@register("jrrp", "exusiaiwei", "一个简单的人品插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-    
+
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+    @filter.command("jrrp")
     async def helloworld(self, event: AstrMessageEvent):
         '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        rp = random.randint(1, 100)
+        if rp <= 50:
+            message_str = "人品不佳，今天的运势不太好哦！"
+        elif rp <= 80:
+            message_str = "人品一般，今天的运势还不错！"
+        elif rp <= 95:
+            message_str = "人品不错，今天的运势很好哦！"
+        else:
+            message_str = "人品爆发，今天的运势非常好！"
+        yield event.plain_result(f"{user_name}, 你今天的人品是{rp}, {message_str}")
 
     async def terminate(self):
         '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
